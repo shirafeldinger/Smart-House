37067ffc46c78c6a996d4921b9aa947c4c4f2bc7
Shira Feldinger <shirafeldinger@gmail.com>
2020-08-08 23:37:35 +0300

    Updates

---
+++ precache-manifest.4d0e1c7614427774fc8f071783cf0056.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b71094ff60a006a65c60b6b884b44bf",
+    "url": "/smart-house/index.html"
+  },
+  {
+    "revision": "f75d48329382d7f64365",
+    "url": "/smart-house/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2299fa021b64f4645fe",
+    "url": "/smart-house/static/css/main.98132ef1.chunk.css"
+  },
+  {
+    "revision": "f75d48329382d7f64365",
+    "url": "/smart-house/static/js/2.89177ef9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smart-house/static/js/2.89177ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2299fa021b64f4645fe",
+    "url": "/smart-house/static/js/main.5eb8e235.chunk.js"
+  },
+  {
+    "revision": "39cca2bc0c6dbd9589de",
+    "url": "/smart-house/static/js/runtime-main.dcb552f8.js"
+  }
+]);
